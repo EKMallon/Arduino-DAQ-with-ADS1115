ba095413be745ad8e0780553a84756a298bb6ed2
EKMallon <EKMallon@users.noreply.github.com>
2016-08-20 11:47:07 -0500

    comments updated

--- UNOonly_DAQ_PeaksOrLeakAverage/UNOonly_DAQ_PeaksOrLeakAverage.ino
+++ UNOonly_DAQ_PeaksOrLeakAverage/UNOonly_DAQ_PeaksOrLeakAverage.ino
@@ -1,6 +1,4 @@
-#include <Wire.h>
-
-// UNO tethered to USB. Note that the ADC is set to use the internal 1.1volt reference so do not apply higher voltages to the analog lines.
+// For an UNO DAQ tethered to USB. Note that the ADC is set to use the internal 1.1volt reference so do not apply higher voltages to the analog lines.
 // Observe the output with the serial monitor enabled, to establish what your
 // your sampling "LoopThreshold" should be in order to capture "events" that cause the current to rise
 
@@ -9,7 +7,8 @@
 #define OVRsamples 5     //The number of samples that get 'averaged' to produce one tick on the serial plotter
 //setting the OVRsample value to 1 (no oversampling) means only 60ms of time are displayed on the plot, values near 17 display around 1 second of time on the plot
 #define LoopThreshold 3  //the "trigger" that starts a sampling loop: change this to something just slightly above the normal resting/sleeping current before the event
-//#define TEXToutput     //enable this define for text-only output & the elapsed time for your reading loop
+//#define TEXToutput     //un-comment this define statement for text-only RAW output with the elapsed time for your reading loop
+                         // the RAW reading output lets you figure out how to set you trigger threshold
 
 int16_t start;
 int16_t elapsed;
@@ -20,51 +19,51 @@ int ADCoffset=2;  // this is a manual fudge factor - my ADC was reading 2 bit to
 uint16_t tempread[OVRsamples];  
 int Peakflag = 0; 
  
-static uint32_t filtsum;
-int shift=1;
+static uint32_t filtsum;  // used in the leaky integrator filter
+int shift=1;              // if you increase the shift value, the readings get smoothed more heavily
+                          // but I rarely use a value above 1 or 2
 
 float scalefactor = 1.07421F; // this is the ADC resolution/bit AFTER setting to the internal 1.1vref
-float millivolts = 0.0; // The result of applying the scale factor to the raw value
-int microamps = 0; 
+float millivolts = 0.0;       // The result of applying the scale factor to the raw ADC value
+int microamps = 0;            // only relevant to this particular application
 
 void setup(void)
 {
   Serial.begin(115200);  
   Wire.begin();
-  analogReference(INTERNAL);  //this sets aref to 1.1v - do not apply higher voltages to the A0 line.
+  analogReference(INTERNAL);  //this sets the ADC aref voltage to 1.1v - do not apply higher voltages to the A0 line.
   
 }
 
 void loop(void)
 {  
 
-if(analogRead(analogPin) >= LoopThreshold){ //rapid sampling loop to capture the event
+if(analogRead(analogPin) >= LoopThreshold){    //the trigger that starts the rapid sampling loop
 
    start=millis();   
       for (int Cycle = 0; Cycle < ADCcycles; Cycle++) {  //rapid ADC sampling loop
 
              Peakflag=0; currentADCreadings[Cycle]=previousADCvalue;
              
-             for (int i = 0; i < OVRsamples; i++) {                          //over sampling loop
-              tempread[i] = analogRead(analogPin); 
-              filteredADCvalue= filter(tempread[i]);
-               if(tempread[i]>currentADCreadings[Cycle]){         // always gives preference to a higher reading
-                currentADCreadings[Cycle]=tempread[i];Peakflag=1;  
+             for (int i = 0; i < OVRsamples; i++) {   // over sampling loop
+              tempread[i] = analogRead(analogPin);                   // the raw reading
+              filteredADCvalue= filter(tempread[i]);                 // the raw reading added to the running average
+               if(tempread[i]>currentADCreadings[Cycle]){            // a higher reading always becomes the selected reading
+                currentADCreadings[Cycle]=tempread[i];Peakflag=1;    // sets a flag so we know a peak event occured
                 }         
                }
               
-              if(Peakflag==0){  //if no short peaks were detected
-                //currentADCreadings[Cycle]=tempread[OVRsamples-1]; //choose this if you just want the last reading to become the new reading
+              if(Peakflag==0){                                      //this only happens if no peaks were detected during the interval
+                //currentADCreadings[Cycle]=tempread[OVRsamples-1]; //choose this if you only want the last reading in a series to become the new reading
                 currentADCreadings[Cycle]=filteredADCvalue;         //choose this if you want the leaky average of the over sampled readings to become the new reading
                 }
-              previousADCvalue=currentADCreadings[Cycle];
-              //delayMicroseconds(10);  //no longer any need for delays, just set your oversampling until you have the time base you want
+              previousADCvalue=currentADCreadings[Cycle];  // saves this interval reading as a starting point for the next one
               
       }  // end of rapid sampling loop  
    elapsed=millis()-start;// elapsed gives you the timebase for your samples
 
 #ifdef TEXToutput
-    for (int Cycle = 0; Cycle < ADCcycles; Cycle++) {  //output of the sample loop for the serial text monitor
+    for (int Cycle = 0; Cycle < ADCcycles; Cycle++) {  //output of the RAW values from the sample loop to the serial text monitor
     Serial.println(currentADCreadings[Cycle]); 
     }  
     //After seeing the raw readings, you have some idea how set your LoopThreshold value
@@ -72,7 +71,7 @@ if(analogRead(analogPin) >= LoopThreshold){ //rapid sampling loop to capture the
 #endif
 
 
-#ifndef TEXToutput  //plotter only output
+#ifndef TEXToutput  //for serial plotter only output
 //Once you get your threshold set properly, you can view your output values on the serial plotter
     for (int Cycle = 0; Cycle < ADCcycles; Cycle++) {  //output of the sample loop to the serial plotter
     Serial.print(4000); Serial.print(" ");//this constant sets a stable upper value line to keep the plotter from rescaling - change this to suit your situation
@@ -80,7 +79,8 @@ if(analogRead(analogPin) >= LoopThreshold){ //rapid sampling loop to capture the
     Serial.print(200); Serial.print(" "); //You can put a line anywhere on the graph you want 
     Serial.print(400); Serial.print(" "); //You can put a line anywhere on the graph you want
 
-    //Here I am converting the output to μA before sending it to the serial plotter
+    //Here I am doing a calculation to convertthe output to μA before sending it to the serial plotter
+    //Your application would be doing different calculations, suitable to your situation.
     millivolts=((currentADCreadings[Cycle]+ADCoffset)*scalefactor);
     microamps=int((millivolts/10.5)*1000); //our shunt resistor was 10.5 ohms
     //microamps= filter(microamps); 
