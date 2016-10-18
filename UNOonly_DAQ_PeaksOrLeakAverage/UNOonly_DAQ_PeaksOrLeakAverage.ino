81236131860e887faf483f231e66d79a097df193
EKMallon <EKMallon@users.noreply.github.com>
2016-10-18 16:18:57 -0500

    Add comments
    Add comments

--- UNOonly_DAQ_PeaksOrLeakAverage/UNOonly_DAQ_PeaksOrLeakAverage.ino
+++ UNOonly_DAQ_PeaksOrLeakAverage/UNOonly_DAQ_PeaksOrLeakAverage.ino
@@ -4,38 +4,46 @@
 
 #define analogPin 0
 #define ADCcycles 450    //You can output 500 samples to the serial plotter, but I find that using a slightly lower number looks nicer
-#define OVRsamples 5     //The number of samples that get 'averaged' to produce one tick on the serial plotter
-//setting the OVRsample value to 1 (no oversampling) means only 60ms of time are displayed on the plot, values near 17 display around 1 second of time on the plot
-#define LoopThreshold 3  //the "trigger" that starts a sampling loop: change this to something just slightly above the normal resting/sleeping current before the event
-//#define TEXToutput     //un-comment this define statement for text-only RAW output with the elapsed time for your reading loop
-                         // the RAW reading output lets you figure out how to set you trigger threshold
+#define OVRsamples 4     //minumum value=1, The number of samples that get 'averaged' to produce one tick on the serial plotter
+//setting the OVRsample value to 1 (no oversampling) means only 60ms of time are displayed on the 500 line plot, values near 17 display around 1 second of time on the plot
+#define LoopThreshold 20  //the "trigger" that starts a sampling loop: change this to something just slightly above the normal resting/sleeping current before the event
+
+// you find out what the resting state reading  is by first observing the output with the serial TEXT monitor:
+//#define TEXToutput     
+//un-comment this define statement for text-only RAW output with the elapsed time for your reading loop
+// the RAW reading output lets you figure out how to set you trigger threshold
 
 int16_t start;
 int16_t elapsed;
-uint16_t currentADCreadings[ADCcycles];  
+uint16_t currentADCreadings[ADCcycles];  //array variable to hold up to 500 samples for serial plotter output
 int16_t filteredADCvalue; 
 int16_t previousADCvalue; 
-int ADCoffset=2;  // this is a manual fudge factor - my ADC was reading 2 bit too low.
-uint16_t tempread[OVRsamples];  
-int Peakflag = 0; 
- 
-static uint32_t filtsum;  // used in the leaky integrator filter
-int shift=1;              // if you increase the shift value, the readings get smoothed more heavily
-                          // but I rarely use a value above 1 or 2
+int ADCoffset=2;                     // this is a manual fudge factor - my ADC was reading 2 bit too low.
+uint16_t tempread[OVRsamples];           //array variable to hold the number of ADC readings you are averaging together
+int Peakflag = 0;                    //peak flag is set to 1/TRUE if a brief spike in readings was detected                  
 
 float scalefactor = 1.07421F; // this is the ADC resolution/bit AFTER setting to the internal 1.1vref
 float millivolts = 0.0;       // The result of applying the scale factor to the raw ADC value
-int microamps = 0;            // only relevant to this particular application
+float shuntResistor =20.8;    // the value of the shunt resistor being used to track the current
+int microamps = 0;            // only relevant to the calculations I am doing
+
+ // variables used in the leaky integrator filter:
+uint32_t filtsum;  
+int shift=1;                        // if you increase the shift value, the readings get smoothed more heavily
+                                    // but I rarely use a value above 1 or 2
 
 void setup(void)
 {
-  Serial.begin(115200);  
-  analogReference(INTERNAL);  //this sets the ADC aref voltage to 1.1v - do not apply higher voltages to the A0 line.
+  Serial.begin(250000);  
+  analogReference(INTERNAL);  //this sets the ADC aref voltage to 1.1v - do not try to read > 1.1v on the A0 line.
   
 }
 
 void loop(void)
 {  
+
+// ADC RAPID SAMPLING LOOP:
+//-------------------------
 
 if(analogRead(analogPin) >= LoopThreshold){    //the trigger that starts the rapid sampling loop
 
@@ -46,56 +54,65 @@ if(analogRead(analogPin) >= LoopThreshold){    //the trigger that starts the rap
              
              for (int i = 0; i < OVRsamples; i++) {   // over sampling loop
               tempread[i] = analogRead(analogPin);                   // the raw reading
-              filteredADCvalue= filter(tempread[i]);                 // the raw reading added to the running average
-               if(tempread[i]>currentADCreadings[Cycle]){            // a higher reading always becomes the selected reading
-                currentADCreadings[Cycle]=tempread[i];Peakflag=1;    // sets a flag so we know a peak event occured
-                }         
+              filteredADCvalue= filter(tempread[i]);                 // current raw reading added to the running average
+               if(tempread[i]>=currentADCreadings[Cycle]){            // a higher reading always becomes the selected reading
+                currentADCreadings[Cycle]=tempread[i];Peakflag=1;     // sets a flag so we know a peak event occured so not using runing average
+                //filtsum=tempread[i];
+               }                       
                }
               
-              if(Peakflag==0){                                      //this only happens if no peaks were detected during the interval
+              if(Peakflag==0){   //this only happens if no peaks were detected during the interval
                 //currentADCreadings[Cycle]=tempread[OVRsamples-1]; //choose this if you only want the last reading in a series to become the new reading
                 currentADCreadings[Cycle]=filteredADCvalue;         //choose this if you want the leaky average of the over sampled readings to become the new reading
                 }
-              previousADCvalue=currentADCreadings[Cycle];  // saves this interval reading as a starting point for the next one
+              previousADCvalue=currentADCreadings[Cycle];  // saves the last interval reading as a starting point for the next one
               
       }  // end of rapid sampling loop  
    elapsed=millis()-start;// elapsed gives you the timebase for your samples
 
-#ifdef TEXToutput
+// OUTPUT for()LOOPS:
+//--------------------
+//This program has two separate output loops, depending on wether you want to view the output in the serial monitor, or the serial plotter
+
+#ifdef TEXToutput  //if TEXToutput IS defined at the beginning of the program , then output includes the SAMPLING elapsed time info
     for (int Cycle = 0; Cycle < ADCcycles; Cycle++) {  //output of the RAW values from the sample loop to the serial text monitor
     Serial.println(currentADCreadings[Cycle]); 
     }  
     //After seeing the raw readings, you have some idea how set your LoopThreshold value
     Serial.print(F(",Time for "));Serial.print(ADCcycles);Serial.print(F(" readings: "));Serial.print(elapsed);Serial.println(F(" milliseconds"));
-#endif
+    Serial.println(F("Note:the Serial Plotter always displays 500 readings, no matter how many you send"));
+#endif  // End of Serial Text Monitor output
 
-
-#ifndef TEXToutput  //for serial plotter only output
+#ifndef TEXToutput  //if TEXToutput is NOT defined at the beginning of the program , then output is for serial plotter ONLY
 //Once you get your threshold set properly, you can view your output values on the serial plotter
     for (int Cycle = 0; Cycle < ADCcycles; Cycle++) {  //output of the sample loop to the serial plotter
-    Serial.print(4000); Serial.print(" ");//this constant sets a stable upper value line to keep the plotter from rescaling - change this to suit your situation
-    Serial.print(0); Serial.print(" ");  //this constant sets a stable lower value for the plotter
-    Serial.print(200); Serial.print(" "); //You can put a line anywhere on the graph you want 
-    Serial.print(400); Serial.print(" "); //You can put a line anywhere on the graph you want
+    Serial.print(7000); Serial.print(" "); // the largest constant sets a stable upper value line to keep the plotter from rescaling
+    Serial.print(5000); Serial.print(" "); // my expected peak current~ 5mA
+    //Serial.print(400); Serial.print(" ");  // You can put a line anywhere to intersect the graph output
+    Serial.print(200); Serial.print(" ");  // this was the typical sleep current for the logger being tested (according to DVM)
+    Serial.print(0); Serial.print(" ");    // the lowest constant sets a stable lower value for the plotter
 
-    //Here I am doing a calculation to convertthe output to μA before sending it to the serial plotter
-    //Your application would be doing different calculations, suitable to your situation.
+    //Here I am doing a calculation to convert the output to μA before sending it to the serial plotter
+    //Your application WOULD BE DOING DIFFERENT CALCULATIONS, suitable to your situation.
     millivolts=((currentADCreadings[Cycle]+ADCoffset)*scalefactor);
-    microamps=int((millivolts/10.5)*1000); //our shunt resistor was 10.5 ohms
+    microamps=int((millivolts/shuntResistor)*1000);     //milivolts is divided by the value of your shunt resistor in ohms
     //microamps= filter(microamps); 
     Serial.println(microamps); 
-    } // End of plotter output 
-
-#endif
+    } 
+#endif // End of Serial Plotter output 
    
-} // end of the threshold triggered sampling loop
+} // end of the threshold triggered sampling loop:   if(analogRead(analogPin) >= LoopThreshold){ 
 
-}  //end of main Loop
+}  //end of MAIN  void loop(void)
 
-// Leaky integrator filter
+
+// Leaky integrator filter - chosen because of low compuation overhead
 // see http://www.bot-thoughts.com/2013/07/oversampling-decimation-filtering.html for more info on the leaky integrator filter
 uint16_t filter(uint16_t value) {
         filtsum += (value - (filtsum >> shift));
         return (filtsum >> shift);
 }
 
+// or you could try other moving average filters
+// http://electronics.stackexchange.com/questions/42734/fastest-moving-averaging-techniques-with-minimum-footprint
+// but they usually involve more computational overhead, increasing the changes of missing a brief current peak...
